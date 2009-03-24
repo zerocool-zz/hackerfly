c3ce73c1c192cbc4ccef77cb0b460eb6ba572127
mendoza <mark_zuckerberg@rocketmail.com>
2009-03-24 01:29:48 -0700

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-# Schema version: 2008100601002
-#
-# Table name: users
-#
-#  id                        :integer       not null, primary key
-#  login                     :string(255)   
-#  crypted_password          :string(40)    
-#  salt                      :string(40)    
-#  created_at                :datetime      
-#  updated_at                :datetime      
-#  remember_token            :string(255)   
-#  remember_token_expires_at :datetime      
-#  is_admin                  :boolean       
-#  can_send_messages         :boolean       default(TRUE)
-#  email_verification        :string(255)   
-#  email_verified            :boolean       
-#
-
 # == schema info
 # schema ver: 
 #
